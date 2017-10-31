65b121f8ba37eb81c9ea4873e49a0d691b6c916b
Alan Pieczonka <alanpieczonka@gmail.com>
2017-11-01 00:05:09 +0100

    Generate component recipe-delete

--- app/components/recipe-single.js
+++ app/components/recipe-single.js
@@ -3,7 +3,7 @@ import Component from '@ember/component';
 const BlogPostComponent = Component.extend({});
 
 BlogPostComponent.reopenClass({
-  positionalParams: ['imageURL', 'name'] //positional params
+  positionalParams: ['imageURL', 'name'] 
 });
 
 export default BlogPostComponent;
