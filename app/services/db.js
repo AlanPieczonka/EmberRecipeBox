e2326708bcac1160152100991bf7a057f1a4de53
Alan Pieczonka <alanpieczonka@gmail.com>
2017-10-31 15:26:40 +0100

    Add db service

---
+++ app/services/db.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+const initialRecipes = [
+    {
+        id: 1,
+        name: 'Pizza',
+        ingredients: 'Ham, tomato, cheese',
+        imageURL: 'https://2.bp.blogspot.com/-K7Q8Fyl4r5U/VsT-pn9R6XI/AAAAAAAA39I/tVaT7aHOzLQ/s1600/pizza.gif'
+    },
+    {
+        id: 2,
+        name: 'Lasagne',
+        ingredients: 'Pasta, meat, vegetables',
+        imageURL: 'https://scm-assets.constant.co/scm/unilever/e9dc924f238fa6cc29465942875fe8f0/28f996d1-0821-44ec-97de-2de86efa897c.jpg'
+    }
+];
+
+export default Ember.Service.extend({
+});
