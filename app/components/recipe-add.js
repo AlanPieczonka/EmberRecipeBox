1e3dd7ba625c8dc73b18268513d48f751e4cb51e
Alan Pieczonka <alanpieczonka@gmail.com>
2017-10-31 20:03:16 +0100

    addRecipe action in recipe-add component

--- app/components/recipe-add.js
+++ app/components/recipe-add.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
+import { inject as service } from '@ember/service';
+
 export default Ember.Component.extend({
+    db: service(),
+    actions: {
+        addRecipe(){
+             let recipe = this.getProperties('name', 'ingredients', 'imageURL');
+             this.get('db').addRecipe(recipe);
+             this._resetForm();
+             /*
+             const lastID = this.get('db').getLastId();
+             console.log('last id ' + lastID);
+             this.get('switchRoute')(lastID);
+             */
+            },
+    },
+    _resetForm(){
+        this.setProperties({
+            name: null,
+            ingredients: null,
+            imageURL: null,
+        });
+    }
 });
