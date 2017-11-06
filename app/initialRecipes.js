2000847d920a9a6d844c912b2a8c80893400f8c7
Alan Pieczonka <alanpieczonka@gmail.com>
2017-11-06 19:16:17 +0100

    Move initial recipes to seperate file

---
+++ app/initialRecipes.js
@@ -0,0 +1,24 @@
+const initialRecipes = [
+    {
+      name: 'Sushi',
+      ingredients: 'Rice, Wasabi, Soy Sauce',
+      imageURL: 'https://wallpaperscraft.com/image/sushi_rolls_meat_fish_plate_platter_71618_1920x1080.jpg',
+    },
+    {
+      name: 'Greek salad',
+      ingredients: 'Lemon, olive oil, cucumber',
+      imageURL: 'http://img.taste.com.au/rLfCKvm4/taste/2016/11/super-easy-greek-salad-81664-1.jpeg',
+    },
+    {
+      name: 'Lasagne',
+      ingredients: 'Pasta, Meat, Vegetables',
+      imageURL: 'http://img.taste.com.au/T2ZPkD17/taste/2016/11/pumpkin-spinach-and-lentil-lasagne-96693-1.jpeg',
+    },
+    {
+      name: '',
+      ingredients: '',
+      imageURL: ''
+    } 
+  ]
+
+export default initialRecipes;
