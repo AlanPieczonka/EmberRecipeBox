8a76ab7e5778a7997064f61fee53b5022cd20886
Alan Pieczonka <alanpieczonka@gmail.com>
2017-11-09 14:55:49 +0100

    Destroy recipe/add and recipe/delete controllers

--- app/controllers/recipes/show.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-    actions: {
-        switchRoute(route){
-            this.transitionToRoute(`/${route}`);
-        }
-    }
-});
